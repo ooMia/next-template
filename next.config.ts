8655d0383507712578fa9922ee0f995e5a72be88
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-02-21 03:33:52 +0900

    🔥 Remove : init to next15

--- next.config.ts
+++ next.config.ts
@@ -1,45 +1,7 @@
-// @ts-check
 import type { NextConfig } from "next";
-import {
-  PHASE_DEVELOPMENT_SERVER,
-  PHASE_PRODUCTION_BUILD,
-} from "next/constants";
 
-const nextConfig = (
-  phase: string,
-  { defaultConfig }: { defaultConfig: NextConfig },
-): NextConfig => {
-  // TODO: use environment variables
-
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      ...defaultConfig,
-      async rewrites() {
-        return [
-          {
-            source: "/api/:path*",
-            destination: "/api/:path*",
-          },
-        ];
-      },
-    };
-  }
-
-  if (phase === PHASE_PRODUCTION_BUILD) {
-    return {
-      ...defaultConfig,
-      async rewrites() {
-        return [
-          {
-            source: "/api/:path*",
-            destination: "/api/:path*",
-          },
-        ];
-      },
-    };
-  }
-
-  return defaultConfig;
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
